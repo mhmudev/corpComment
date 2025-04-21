84d1e666228c0e4d2aa730b4976ab009154d9950
Mahmoud Attia <attiamahmoud222@gmail.com>
2025-04-21 10:40:24 +0200

    controlled textarea

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+import { MAX_CHARS } from "../lib/constants";
+
 export default function FeedbackForm() {
+  const [text, setText] = useState("");
+  const charLeft = MAX_CHARS - text.length;
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const newText = e.target.value;
+    if (newText.length > MAX_CHARS) {
+      return;
+    }
+    setText(newText);
+  };
+
   return (
     <form className="form">
       <textarea
+        value={text}
+        onChange={handleChange}
         id="feedback-textarea"
         placeholder="blabla"
         spellCheck={false}
@@ -12,7 +28,7 @@ export default function FeedbackForm() {
       </label>
 
       <div>
-        <p className="u-italic">150</p>
+        <p className="u-italic">{charLeft}</p>
         <button>
           <span>Submit</span>
         </button>
