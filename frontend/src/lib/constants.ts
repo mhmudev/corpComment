7541f48c20a0d9d8a4dd18b254949f91c862b9c2
Mahmoud Attia <attiamahmoud222@gmail.com>
2025-05-09 16:50:29 +0300

    api base url

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -1,4 +1,4 @@
 const MAX_CHARS = 150;
-const API_BASE_URL = "https://corvey-backend-production.up.railway.app";
+const API_BASE_URL = "https://corpcomment-production.up.railway.app";
 
 export { MAX_CHARS, API_BASE_URL };
